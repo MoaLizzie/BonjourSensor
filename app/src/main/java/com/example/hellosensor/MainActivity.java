8528e80c6205ad74fafcf2bceb46050f169dabb8
Moa <mo0353sa@student.lu.se>
2024-01-24 20:53:02 +0100

    added buttons

--- app/src/main/java/com/example/hellosensor/MainActivity.java
+++ app/src/main/java/com/example/hellosensor/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.hellosensor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +15,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void toggle (View v) {
+        Button b = (Button) v;
+        b.setEnabled(false);
+    }
 }
